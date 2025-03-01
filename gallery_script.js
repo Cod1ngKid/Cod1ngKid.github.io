e06c90dc699f59bd22cc95793f9369bd8e1e221d
your_username <your_email@your_domain.com>
2025-03-01 11:32:14 -0800

    Added Gallery Script

---
+++ gallery_script.js
@@ -0,0 +1,5 @@
+const imageGallery = [
+    "https://image.api.playstation.com/vulcan/ap/rnd/202407/0401/670c294ded3baf4fa11068db2ec6758c63f7daeb266a35a1.png?w=440",
+    "https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/screenshots/EXPLORE_PDPScreenshotRefresh2024_multipleBiomes_01.png",
+    "https://assetsio.gnwcdn.com/things-to-build-in-minecraft-floating-base.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp",
+]
