d9584bb5454be69a30794ed995b264a83fd8649f
your_username <your_email@your_domain.com>
2025-03-08 11:34:10 -0800

    Coloring scheme added

--- gallery_script.js
+++ gallery_script.js
@@ -3,7 +3,7 @@ const imageGallery = [
     "https://www.minecraft.net/content/dam/minecraftnet/games/minecraft/screenshots/EXPLORE_PDPScreenshotRefresh2024_multipleBiomes_01.png",
     "https://assetsio.gnwcdn.com/things-to-build-in-minecraft-floating-base.jpg?width=1200&height=1200&fit=bounds&quality=70&format=jpg&auto=webp",
     "https://preview.redd.it/795imv7btfy51.png?width=640&crop=smart&auto=webp&s=50d0109d5b2bce64d46b699398f33180fa3b70d0",
-    "https://static.wikia.nocookie.net/minecraft-mob/images/7/77/Biomes.jpg/revision/latest?cb=20230305194548",
+    "https://static.wikia.nocookie.net/minecraft-mob/images/7/77/Biomes.jpg/revision/latest?cb=20230305194548"
     ]
 
 const imageElement = document.getElementById("gallery-image");
